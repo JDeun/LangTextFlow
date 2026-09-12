756225fe495c2776eccb258d9bff7a143fa3b77e
Kevin. Cho <165640725+JDeun@users.noreply.github.com>
2026-09-12 13:14:56 +0900

    feat: surface preflight on operator UI

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { PreflightPanel } from "./PreflightPanel";
 import "./styles.css";
+
+const isOperatorRoute = !/^\/(audience|display)\//.test(window.location.pathname);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    {isOperatorRoute && <PreflightPanel />}
     <App />
   </StrictMode>,
 );
